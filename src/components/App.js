f983bdcabd116c0fb79170d6879f60b29ed8a6a0
Alex Kiborgok <borgokalek14@gmail.com>
2022-04-29 12:15:49 +0300

    extract components into their individual modules

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,7 @@
 import React from "react";
-
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-{/* write an <About> component here */ }
-function About() {
-  return (
-    <div id="about">
-      <h2>About</h2>
-    </div>
-  );
-}
-
+import NavBar from "./NavBar";
+import About from "./About";
+import Home from "./Home";
 
 function App() {
   return (
